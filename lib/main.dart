97d6ff3315c7b74e026af1500198663944132e58
Twin Edo <venganzdektor26@gmail.com>
2021-03-14 15:55:10 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:test_flutter_web/pages/landing_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LandingPage());
+  }
+}
